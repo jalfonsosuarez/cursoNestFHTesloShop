625bb00098495f74db93b1192b1bfbfee8787421
= <=>
2022-07-31 19:46:39 +0200

    Upload file

---
+++ src/files/files.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+}
