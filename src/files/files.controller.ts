93664023ed2df81733c60c4714fc017c8c14c4b5
= <=>
2022-07-31 21:08:38 +0200

    Serve files from contolled mode.

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,19 +1,49 @@
 import {
+  BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: {
+      //   fileSize: 100,
+      // },
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file)
+      throw new BadRequestException('Make sure that the file is an image.');
+
+    const secureUrl = `${file.filename}`;
+
+    return { secureUrl };
+  }
+
+  @Get('product/:imageName')
+  findProductImg(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
   }
 }
