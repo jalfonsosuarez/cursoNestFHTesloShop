e7c3ac474ea51d09874ae3714da1ad0da6431f6a
= <=>
2022-08-02 21:02:18 +0200

    Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { Auth, GetUser, RawHeaders } from './decorators';
+import { RoleProtected } from './decorators/role-protected.decorator';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -16,8 +22,26 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-  @Get()
-  testPrivateRoute() {
-    return 'Hola esto es una ruta privada.';
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+  ) {
+    return { user, userEmail, rawHeaders };
+  }
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  private2(@GetUser() user: User) {
+    return user;
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.admin)
+  private3(@GetUser() user: User) {
+    return user;
   }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,7 +13,9 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from '../common/dtos/pagination.dto';
+import { Auth } from '../auth/decorators';
 @Controller('Products')
+@Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
