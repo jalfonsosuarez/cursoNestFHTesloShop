4ceaea761f734070ae900be3b54971ad9270a775
= <=>
2022-08-05 19:52:35 +0200

    Documentation part 1

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'Rows do you need.',
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'Rows do you whant skip.',
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number)
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,12 +16,22 @@ import { PaginationDto } from '../common/dtos/pagination.dto';
 import { Auth, GetUser } from '../auth/decorators';
 import { ValidRoles } from '../auth/interfaces/valid-roles';
 import { User } from '../auth/entities/user.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Product } from './entities';
+@ApiTags('Products')
 @Controller('Products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
   @Auth(ValidRoles.admin)
+  @ApiResponse({
+    status: 201,
+    description: 'Product was created',
+    type: Product,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 403, description: 'Forbbiden. Token related.' })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(createProductDto, user);
   }
