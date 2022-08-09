576673ea08b2b021e4e12ad38d921770ab1a316f
= <=>
2022-08-09 18:38:55 +0200

    Configuraciones despliegue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,16 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
 import { MessagesWsModule } from './messages-ws/messages-ws.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE == 'prod',
+      extra: {
+        ssl: process.env.STAGE == 'prod' ? { rejectUnauthorized: false } : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
